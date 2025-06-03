6ea3846aa06c5580fa027461390dc5839b652a0a
Lomudru <kmejasson@gmail.com>
2025-06-03 21:56:12 +0200

    Modification des routes pour les cartes et les saison : - Route /carte/nom/{nom} enlever - Route /carte/saison/{saison} enlever - Route /carte/saison/{saison}/nom/{nom} enlever - Route /carte/effigie/{effigie} enlever - Route /carte/artiste/{artiste} enlever - Route /carte/rarete/{rarete} enlever - Route /saison/nom/{nom} enlever - Route /saison/numero/{numero} enlever - Route /cartes modifier pour rajouter les query params - Route /saison modifier pour rajouter les query params Mise a jour du swagger suite au modification des routes

--- src/db.ts
+++ src/db.ts
@@ -27,74 +27,65 @@ const getDbPool = () => {
 // Fonctions pour récupérer les cartes
 // ===================================
 
-export async function getCartes() {
+export async function getCartes(saison_nom?: string,saison_numero?: string, nom?: string , numero?: string, effigie?: string, artiste?: string, rarete?: string) {
+    let queryAddition = '';
+    let queryParams: string[] = [];
+
+    if (saison_nom) {
+        queryAddition += 'AND s.nom = ? ';
+        queryParams.push(saison_nom);
+    }
+    if (saison_numero) {
+        queryAddition += 'AND s.numero = ? ';
+        queryParams.push(saison_numero);
+    }
+    if (nom) {
+        queryAddition += 'AND ci.titre LIKE ? ';
+        queryParams.push(`%${nom}%`);
+    }
+    if (numero) {
+        queryAddition += 'AND ci.numero = ? ';
+        queryParams.push(numero);
+    }
+    if (effigie) {
+        queryAddition += 'AND e.nom = ? ';
+        queryParams.push(effigie);
+    }
+    if (artiste) {
+        queryAddition += 'AND a.nom = ? ';
+        queryParams.push(artiste);
+    }
+    if (rarete) {
+        queryAddition += 'AND r.nom = ? ';
+        queryParams.push(rarete);
+    }
+
     const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id  UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC");
+    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE 1 "+ queryAddition +" UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE 1 "+ queryAddition +" ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC", queryParams.concat(queryParams));
     return rows;
 }
-
-export async function getCartesBySaison(saison: string){
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE s.nom = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE s.nom = ? ORDER BY CONVERT(numero, SIGNED INTEGER) ASC", [saison, saison]);
-    return rows;
-}
-
-export async function getCarteByNom(nom: string) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.titre LIKE ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.titre LIKE ? ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC", [nom, nom]);
-    return rows;
-}
-
-export async function getCarteByNumero(numero: number) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.numero = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.numero = ? ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC", [numero.toString(), numero.toString()]);
-    return rows;
-}
-
-export async function getCarteByNumeroAndSaison(numero: number, saison: number) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.numero = ? AND s.numero = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE ci.numero = ? AND s.numero = ?", [numero.toString(), saison, numero.toString(), saison]);
-    return rows;
-}
-
-export async function getCarteByEffigie(effigie: string) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE e.nom = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE e.nom = ? ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC", [effigie, effigie]);
-    return rows;
-}
-
-export async function getCarteByArtiste(artiste: string) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE a.nom = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE a.nom = ? ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC", [artiste, artiste]);
-    return rows;
-}
-
-export async function getCarteByRarete(rarete: string) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT carte image, regle, combo, citation, NULL effet_gagnant, NULL effet_perdant, NULL effet_special, titre, s.nom saison, s.numero numero_saison, ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM carte c LEFT JOIN carte_info ci ON c.carte_info=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE r.nom = ? UNION ALL SELECT carte image, NULL regle, NULL combo, NULL citation, effet_gagnant, effet_perdant, effet_special, titre, s.nom saison, s.numero numero_saison , ci.numero numero, e.nom effigie, a.nom artiste, r.nom rarete, r.drop taux_de_drop, cout, `force` FROM terrain t LEFT JOIN carte_info ci ON t.info_carte=ci.id LEFT JOIN saison s ON ci.saison=s.id LEFT JOIN effigie e ON ci.effigie=e.id LEFT JOIN artiste a ON ci.artiste=a.id LEFT JOIN rarete r ON ci.rarete=r.id WHERE r.nom = ? ORDER BY CASE WHEN numero_saison = 0 THEN 1 ELSE 0 END, numero_saison ASC, CONVERT(numero, SIGNED INTEGER) ASC", [rarete, rarete]);
-    return rows;
-}
-
 
 // ====================================
 // Fonctions pour récupérer les saisons 
 // ====================================
 
-export async function getSaisons() {
+export async function getSaisons(saison_nom?: string, saison_numero?: string, nbr_Cartes?: number) {
+    let queryAddition = '';
+    let queryParams: string[] = [];
+    if (saison_nom) {
+        queryAddition += 'AND s.nom = ? ';
+        queryParams.push(saison_nom);
+    }
+    if (saison_numero) {
+        queryAddition += 'AND s.numero = ? ';
+        queryParams.push(saison_numero);
+    }
+    if (nbr_Cartes !== undefined) {
+        queryAddition += 'AND s.nbr_Carte = ? ';
+        queryParams.push(nbr_Cartes.toString());
+    }
     const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT nom saison, numero, nbr_Carte FROM saison s ORDER BY CASE WHEN numero = 0 THEN 1 ELSE 0 END, numero ASC");
-    return rows;
-}
-
-export async function getSaisonByNom(nom: string) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT nom saison, numero, nbr_Carte FROM saison s WHERE nom = ?", [nom]);
-    return rows;
-}
-
-export async function getSaisonByNumero(numero: number) {
-    const dbPool = getDbPool();
-    const [rows] = await dbPool.query("SELECT nom saison, numero, nbr_Carte FROM saison s WHERE numero = ?", [numero]);
+    const [rows] = await dbPool.query(`SELECT nom saison, numero, nbr_Carte FROM saison s WHERE 1 ${queryAddition} ORDER BY CASE WHEN numero = 0 THEN 1 ELSE 0 END, numero ASC`, queryParams);
     return rows;
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express, {Request, Response} from 'express';
-import { getCartes, getCartesBySaison, getSaisons, getSaisonByNom, getSaisonByNumero, getCarteByNom, getCarteByNumero, getCarteByNumeroAndSaison, getCarteByEffigie, getCarteByArtiste, getCarteByRarete, getEffigies, getArtistes, getRaretes } from './db';
+import { getCartes, getSaisons, getEffigies, getArtistes, getRaretes } from './db';
 dotenv.config();
 
 const app = express();
@@ -23,8 +23,19 @@ app.listen(port, () => {
 
 // Recupération de toutes les cartes
 app.get('/cartes', (req: Request, res: Response) => {
-    getCartes()
+    const saison = req.query.saison ? req.query.saison.toString() : undefined;
+    const saison_numero = req.query.saison_numero ? req.query.saison_numero.toString() : undefined;
+    const nom = req.query.nom ? req.query.nom.toString() : undefined;
+    const numero = req.query.numero ? req.query.numero.toString() : undefined;
+    const effigie = req.query.effigie ? req.query.effigie.toString() : undefined;
+    const artiste = req.query.artiste ? req.query.artiste.toString() : undefined;
+    const rarete = req.query.rarete ? req.query.rarete.toString() : undefined;
+
+    getCartes(saison, saison_numero, nom, numero, effigie, artiste, rarete)
         .then(cartes => {
+            if (Array.isArray(cartes) && cartes.length === 0) {
+                return res.status(404).json({ error: 'Aucune carte trouvée' });
+            }
             res.json(cartes);
         })
         .catch(err => {
@@ -33,165 +44,25 @@ app.get('/cartes', (req: Request, res: Response) => {
         });
 });
 
-// Récupération des cartes par saison
-app.get('/cartes/:saison', (req: Request, res: Response) => {
-    const saison = req.params.saison;
-    getCartesBySaison(saison)
-        .then(cartes => {
-            if (Array.isArray(cartes) && cartes.length === 0) {
-                return res.status(404).json({ error: `Aucune carte trouvée pour la saison ${saison}` });
-            }
-            res.json(cartes);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération des cartes pour la saison ${saison}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération des cartes pour la saison ${saison}` });
-        });
-});
-
-// Récupération d'une carte par son nom
-app.get('/cartes/nom/:nom', (req: Request, res: Response) => {
-    const nom = req.params.nom.replace(/_/g, ' '); 
-    getCarteByNom(`%${nom}%`)
-        .then(carte => {
-            if (Array.isArray(carte) && carte.length === 0) {
-                return res.status(404).json({ error: `Carte ${nom} non trouvée` });
-            }
-            res.json(carte);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération de la carte ${nom}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération de la carte ${nom}` });
-        });
-});
-
-// Recupération d'une carte par son numéro
-app.get('/cartes/numero/:numero', (req: Request, res: Response) => {
-    const numero = parseInt(req.params.numero);
-    getCarteByNumero(numero)
-        .then(carte => {
-            if (Array.isArray(carte) && carte.length === 0) {
-                return res.status(404).json({ error: `Carte numéro ${numero} non trouvée` });
-            }
-            res.json(carte);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération de la carte numéro ${numero}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération de la carte numéro ${numero}` });
-        });
-});
-
-// Recupération d'une carte par son numéro et sa saison
-app.get('/cartes/saison/:saison/numero/:numero', (req: Request, res: Response) => {
-    const numero = parseInt(req.params.numero);
-    const saison = parseInt(req.params.saison);
-    getCarteByNumeroAndSaison(numero, saison)
-        .then(carte => {
-            if (Array.isArray(carte) && carte.length === 0) {
-                return res.status(404).json({ error: `Carte numéro ${numero} pour la saison ${saison} non trouvée` });
-            }
-            res.json(carte);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération de la carte numéro ${numero} pour la saison ${saison}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération de la carte numéro ${numero} pour la saison ${saison}` });
-        });
-});
-
-// Récupération des cartes par effigie
-app.get('/cartes/effigie/:effigie', (req: Request, res: Response) => {
-    const effigie = req.params.effigie;
-    getCarteByEffigie(effigie)
-        .then(cartes => {
-            if (Array.isArray(cartes) && cartes.length === 0) {
-                return res.status(404).json({ error: `Aucune carte trouvée pour l'effigie ${effigie}` });
-            }
-            res.json(cartes);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération des cartes pour l'effigie ${effigie}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération des cartes pour l'effigie ${effigie}` });
-        });
-});
-
-// Récupération des cartes par artiste
-app.get('/cartes/artiste/:artiste', (req: Request, res: Response) => {
-    const artiste = req.params.artiste.replace(/_/g, ' '); 
-    getCarteByArtiste(artiste)
-        .then(cartes => {
-            if (Array.isArray(cartes) && cartes.length === 0) {
-                return res.status(404).json({ error: `Aucune carte trouvée pour l'artiste ${artiste}` });
-            }
-            res.json(cartes);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération des cartes pour l'artiste ${artiste}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération des cartes pour l'artiste ${artiste}` });
-        });
-});
-
-// Récupération des cartes par rareté
-app.get('/cartes/rarete/:rarete', (req: Request, res: Response) => {
-    const rarete = req.params.rarete.replace(/_/g, ' ');
-    getCarteByRarete(rarete)
-        .then(cartes => {
-            if (Array.isArray(cartes) && cartes.length === 0) {
-                return res.status(404).json({ error: `Aucune carte trouvée pour la rareté ${rarete}` });
-            }
-            res.json(cartes);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération des cartes pour la rareté ${rarete}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération des cartes pour la rareté ${rarete}` });
-        });
-});
-
-
 // ========================
 // Recuperation des saisons 
 // ========================
 
 // Recupération des saisons
 app.get('/saisons', (req: Request, res: Response) => {
-    getSaisons()
+    const saison = req.query.saison ? req.query.saison.toString() : undefined;
+    const saison_numero = req.query.saison_numero ? req.query.saison_numero.toString() : undefined;
+    const nbr_Cartes = req.query.nbr_cartes ? parseInt(req.query.nbr_cartes.toString(), 10) : undefined;
+    getSaisons(saison, saison_numero, nbr_Cartes)
         .then(saisons => {
+            if (Array.isArray(saisons) && saisons.length === 0) {
+                return res.status(404).json({ error: 'Aucune saison trouvée' });
+            }
             res.json(saisons);
         })
         .catch(err => {
             console.error('Erreur lors de la récupération des saisons:', err);
             res.status(500).json({ error: 'Erreur lors de la récupération des saisons' });
-        });
-});
-
-// Recupération d'une saison par son nom
-app.get('/saison/:nom', (req: Request, res: Response) => {
-    const nom = req.params.nom;
-    getSaisonByNom(nom)
-        .then(saison => {
-            if (!saison) {
-                return res.status(404).json({ error: `Saison ${nom} non trouvée` });
-            }
-            res.json(saison);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération de la saison ${nom}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération de la saison ${nom}` });
-        });
-});
-
-// Recupération d'une saison par son numéro
-app.get('/saison/numero/:numero', (req: Request, res: Response) => {
-    const numero = parseInt(req.params.numero);
-    getSaisonByNumero(numero)
-        .then(saison => {
-            if (!saison) {
-                return res.status(404).json({ error: `Saison numéro ${numero} non trouvée` });
-            }
-            res.json(saison);
-        })
-        .catch(err => {
-            console.error(`Erreur lors de la récupération de la saison numéro ${numero}:`, err);
-            res.status(500).json({ error: `Erreur lors de la récupération de la saison numéro ${numero}` });
         });
 });
 
