5040db0c5e3ee4fee176d82068c3ed428f2c98cf
Lomudru <kmejasson@gmail.com>
2025-06-08 23:48:22 +0200

    Modification du texte pour l'appel api

--- front/src/components/pages/Introduction.tsx
+++ front/src/components/pages/Introduction.tsx
@@ -48,8 +48,7 @@ function Intro(){
         <section>
             <h3>Introduction <span></span></h3>
             <p>Wankul API est une API permettant la récupération de toutes les cartes Wankul et autres informations relatives à Wankul</p>
-            <p>Pour faire un appel a l'API il appeler <span className="requete">https://api.wankulapi-unofficial.fr</span></p>
-            <p>Elle renvoie les informations demandées sous forme de JSON</p>
+            <p>Pour faire un appel à l'API, utilisez <span className="requete">https://api.wankulapi-unofficial.fr</span></p>            <p>Elle renvoie les informations demandées sous forme de JSON</p>
             <p>Voici la mise en forme du JSON pour la récupération des cartes</p>
             <pre className="requete">{exempleJsonCarte}</pre>
             <p>Dans le cas où la carte que vous récupérez est une carte terrain, les champs règle, combo et citation seront nuls et le coût et la force seront à 0.</p>
